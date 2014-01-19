cd6b2863c4cf24c4177dc502193c064c4f9a943a
kwmuth <kwmuth@gmail.com>
2014-01-19 11:28:29 -0800

    Added Files

---
+++ Assembly/ADC to Temp/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\Kawindi\Documents\GitHub\Public\Assembly\ADC to Temp\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Kawindi\Documents\GitHub\Public\Assembly\ADC to Temp\ADC to Temperature.asm"
+echo Crosside_Action Set_Hex_File C:\Users\Kawindi\Documents\GitHub\Public\Assembly\ADC to Temp\ADC to Temperature.HEX
---
+++ Assembly/Frequency Meter/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\Kawindi\Documents\GitHub\Public\Assembly\Frequency Meter\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Kawindi\Documents\GitHub\Public\Assembly\Frequency Meter\Frequency Meter.asm"
+echo Crosside_Action Set_Hex_File C:\Users\Kawindi\Documents\GitHub\Public\Assembly\Frequency Meter\Frequency Meter.HEX
